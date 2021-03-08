b7ffdc4223e3a1408c920bf43b5e1fcdbf2015d7
Andrey <dron.donbass@gmail.com>
2021-03-08 12:05:20 +0200

    feat: code human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function toReadable (number) {
-  
+    let ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let digit = number % 10;
+
+    if (number < 20) {
+        return ones[number];
+    }
+    if(number < 100) {
+        return tens[Math.floor(number / 10) - 2] + (digit ? ' ' + ones[digit]: '');
+    }
+    if (number < 1000) {
+        return ones[Math.floor(number / 100)] + " hundred" + (number % 100 === 0 ? "" : " " + toReadable(number % 100));
+    }
 }
